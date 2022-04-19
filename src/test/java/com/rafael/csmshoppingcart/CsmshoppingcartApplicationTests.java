0037ac380cea67d37f7d6a2096708ff466757637
r-afael <roliveira1111@gmail.com>
2022-04-19 20:49:58 -0300

    first commit

---
+++ src/test/java/com/rafael/csmshoppingcart/CsmshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.csmshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmshoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
