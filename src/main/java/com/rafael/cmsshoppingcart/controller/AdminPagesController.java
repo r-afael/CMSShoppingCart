ede2414dcc8de29c5ff28d7fd2ded2e567a9ff93
r-afael <roliveira1111@gmail.com>
2022-04-22 11:55:19 -0300

    feat: add page form validation

--- src/main/java/com/rafael/cmsshoppingcart/controller/AdminPagesController.java
+++ src/main/java/com/rafael/cmsshoppingcart/controller/AdminPagesController.java
@@ -2,24 +2,27 @@ package com.rafael.cmsshoppingcart.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.rafael.cmsshoppingcart.model.data.Page;
 import com.rafael.cmsshoppingcart.model.data.PageRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
     
+    @Autowired
     private PageRepository pageRepo;
-
-    public AdminPagesController( PageRepository pageRepo) {
-        this.pageRepo = pageRepo;
-    }
 
     @GetMapping
     public String index(Model model) {
@@ -35,5 +38,33 @@ public class AdminPagesController {
        // model.addAttribute("page", new Page());
 
         return "admin/pages/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model ) {
+
+        if(bindingResult.hasErrors()){
+            return "admin/pages/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepo.findBySlug(slug);
+
+        if(slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setSlug(slug); 
+            page.setSorting(100);
+
+            pageRepo.save(page);
+        }
+        
+        return "redirect:/admin/pages/add";
     }
 }
