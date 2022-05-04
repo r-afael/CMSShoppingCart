c45ca319806aa88fdcfb60b908d426a473cb0bc5
r-afael <roliveira1111@gmail.com>
2022-05-04 17:30:14 -0300

    feat: implement categories CRUD

---
+++ src/main/java/com/rafael/cmsshoppingcart/model/data/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.rafael.cmsshoppingcart.model.data;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+    
+    Category findByName(String name);
+
+    List<Category> findAllByOrderBySortingAsc();
+}
